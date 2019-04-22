53de1419717be6f1b2391389f9746ebc63db2137
wuwaer <784785282@qq.com>
2019-04-22 13:28:03 +0800

    Add files via upload

---
+++ wxApitest/Object Repository/createrdepart/cretdepartment.rs
@@ -0,0 +1,46 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebServiceRequestEntity>
+   <description></description>
+   <name>cretdepartment</name>
+   <tag></tag>
+   <elementGuidId>d9281297-d0d3-45d0-ba23-6bdaa0a7f765</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <httpBody></httpBody>
+   <httpBodyContent>{
+  &quot;text&quot;: &quot;{\n   \&quot;name\&quot;: \&quot;广州研发中心\&quot;,\n   \&quot;parentid\&quot;: 1,\n   \&quot;order\&quot;: 1,\n   \&quot;id\&quot;: 2\n}&quot;,
+  &quot;contentType&quot;: &quot;application/json&quot;,
+  &quot;charset&quot;: &quot;UTF-8&quot;
+}</httpBodyContent>
+   <httpBodyType>text</httpBodyType>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Content-Type</name>
+      <type>Main</type>
+      <value>application/json</value>
+   </httpHeaderProperties>
+   <migratedVersion>5.4.1</migratedVersion>
+   <restRequestMethod>POST</restRequestMethod>
+   <restUrl>https://qyapi.weixin.qq.com/cgi-bin/department/create?access_token=a1p4cx9t6iaaGc1vl4KO2GX7lgTc6qp9hTMIRLN8TeEVdtSJInQgnEPF0XVU7llEo3Df4U3fFW8NRBMYkHQZNE5Hojx3gyQaJOqiYS_s5DShI1ed8FSDhQwwAN5xApiqaaQgUnIkUO4ioiZSEKbiBnINisJpPqT1jUSJXKgAXbxmuqV8IgMEahyVLMoeGv1vSvAOQmXBnVotsiTqALVC_Q</restUrl>
+   <serviceType>RESTful</serviceType>
+   <soapBody></soapBody>
+   <soapHeader></soapHeader>
+   <soapRequestMethod></soapRequestMethod>
+   <soapServiceFunction></soapServiceFunction>
+   <verificationScript>import static org.assertj.core.api.Assertions.*
+
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webservice.verification.WSResponseManager
+
+import groovy.json.JsonSlurper
+import internal.GlobalVariable as GlobalVariable
+
+RequestObject request = WSResponseManager.getInstance().getCurrentRequest()
+
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()
+</verificationScript>
+   <wsdlAddress></wsdlAddress>
+</WebServiceRequestEntity>
---
+++ wxApitest/Object Repository/getToken/getApiToken.rs
@@ -0,0 +1,55 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebServiceRequestEntity>
+   <description></description>
+   <name>getApiToken</name>
+   <tag></tag>
+   <elementGuidId>a931e741-3cbd-4461-959b-aebb80852323</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <httpBody></httpBody>
+   <httpBodyContent></httpBodyContent>
+   <httpBodyType></httpBodyType>
+   <migratedVersion>5.4.1</migratedVersion>
+   <restRequestMethod>GET</restRequestMethod>
+   <restUrl>${url}?corpid=ww5e2b49a3b4cd4f00&amp;corpsecret=qfvg8Wm3Kt5xDC4XeAU2ezChEwAowVWrLqi1QUhUpIs</restUrl>
+   <serviceType>RESTful</serviceType>
+   <soapBody></soapBody>
+   <soapHeader></soapHeader>
+   <soapRequestMethod></soapRequestMethod>
+   <soapServiceFunction></soapServiceFunction>
+   <variables>
+      <defaultValue>GlobalVariable.wx_url</defaultValue>
+      <description></description>
+      <id>c27f2d21-cfce-4db3-9cd1-26c6ba904ae7</id>
+      <masked>false</masked>
+      <name>url</name>
+   </variables>
+   <verificationScript>import static org.assertj.core.api.Assertions.*
+
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webservice.verification.WSResponseManager
+
+import groovy.json.JsonSlurper
+import internal.GlobalVariable as GlobalVariable
+
+RequestObject request = WSResponseManager.getInstance().getCurrentRequest()
+
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()
+
+
+WS.verifyElementPropertyValue(response, 'errmsg', 'ok')
+def jsonSlurper = new JsonSlurper()
+
+def jsonResponse = jsonSlurper.parseText(respone.getResponseText())
+
+def token = jsonResponse.access_token
+File file = new File(&quot;Data Files/token/getToken&quot;)
+BufferedWriter out = new BufferedWriter(new FileWriter(file))
+out.write(token)
+out.flush()
+out.close()
+</verificationScript>
+   <wsdlAddress></wsdlAddress>
+</WebServiceRequestEntity>
